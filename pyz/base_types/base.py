2de3cc2cc210c9ed12befc15b032f3caa8c8b50e
Jonatan Martens <jonatanmartenstav@gmail.com>
2020-08-21 22:44:46 +0300

    [IMPROVED] Reduced executor

--- pyz/base_types/base.py
+++ pyz/base_types/base.py
@@ -1,3 +1,5 @@
+import os
+
 import grpc
 
 from pyz.grpc_internals.zeebe_pb2_grpc import GatewayStub
@@ -5,21 +7,26 @@ from pyz.grpc_internals.zeebe_pb2_grpc import GatewayStub
 
 class ZeebeBase(object):
     def __init__(self, hostname: str = None, port: int = None, **kwargs):
-        hostname = hostname or 'localhost'
-        port = port or 26500
-        self._channel = grpc.insecure_channel(f'{hostname}:{port}')
+        self._connection_uri = f'{hostname}:{port}' or os.getenv('ZEEBE_HOST') or 'localhost:26500'
+        self._channel = grpc.insecure_channel(self._connection_uri)
         self.connected = False
         self.retrying_connection = True
         self._channel.subscribe(self.__check_connectivity, try_to_connect=True)
         self.zeebe_client = GatewayStub(self._channel)
 
     def __check_connectivity(self, value: str):
-        print(value)
         if value == grpc.ChannelConnectivity.READY:
             self.connected = True
             self.retrying_connection = False
-        elif value != grpc.ChannelConnectivity.CONNECTING:
+        elif value == grpc.ChannelConnectivity.CONNECTING:
             self.connected = False
             self.retrying_connection = True
-        else:
+        elif value == grpc.ChannelConnectivity.TRANSIENT_FAILURE:
             self.connected = False
+            self.retrying_connection = True
+        elif value == grpc.ChannelConnectivity.SHUTDOWN:
+            self.connected = False
+            self.retrying_connection = False
+            raise ConnectionAbortedError(f'Lost connection to {self._connection_uri}')
+        elif value == grpc.ChannelConnectivity.IDLE:
+            pass
--- pyz/worker/worker.py
+++ pyz/worker/worker.py
@@ -1,13 +1,13 @@
 import concurrent.futures
 import json
 import socket
-from typing import Tuple, List, Callable
+from typing import Tuple, List, Callable, Generator
 
 from pyz.base_types.base import ZeebeBase
 from pyz.decorators.base_zeebe_decorator import BaseZeebeDecorator
 from pyz.decorators.task_decorator import TaskDecorator
 from pyz.exceptions import TaskNotFoundException
-from pyz.grpc_internals.zeebe_pb2 import ActivateJobsRequest, CompleteJobRequest, ActivateJobsResponse
+from pyz.grpc_internals.zeebe_pb2 import ActivateJobsRequest, CompleteJobRequest
 from pyz.task.task import Task
 from pyz.task.task_context import TaskContext
 
@@ -31,19 +31,17 @@ class ZeebeWorker(ZeebeBase, BaseZeebeDecorator):
         while self.connected or self.retrying_connection:
             if self.retrying_connection:
                 continue
-            responses = self._get_active_jobs(task)
 
-            for response in responses:
-                for job in response.jobs:
-                    with concurrent.futures.ThreadPoolExecutor(max_workers=len(response.jobs)) as executor:
-                        executor.submit(task.handler, self._create_task_context_from_job(job))
-                    executor.shutdown(wait=True)
+            for context in self._get_task_stream(task):
+                task.handler(context)
 
-    def _get_active_jobs(self, task: Task) -> ActivateJobsResponse:
+    def _get_task_stream(self, task: Task) -> Generator[TaskContext, None, None]:
         request = ActivateJobsRequest(type=task.type, worker=self.name, timeout=task.timeout,
                                       maxJobsToActivate=task.max_jobs_to_activate,
                                       fetchVariable=task.variables_to_fetch, requestTimeout=self.request_timeout)
-        return self.zeebe_client.ActivateJobs(request)
+        for response in self.zeebe_client.ActivateJobs(request):
+            for job in response.jobs:
+                yield self._create_task_context_from_job(job)
 
     @staticmethod
     def _create_task_context_from_job(job) -> TaskContext:
