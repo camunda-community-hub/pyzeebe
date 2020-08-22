befee9a87b3c2e89a9ec8525572873e0b068d83b
Jonatan Martens <jonatanmartenstav@gmail.com>
2020-08-22 23:35:15 +0300

    [ADDED] examples

---
+++ examples/worker.py
@@ -0,0 +1,23 @@
+from typing import Dict
+
+from zeebepy import Task, JobContext, TaskStatusController, ZeebeWorker
+
+
+def example_task(input: str) -> Dict:
+    return {'output': f'Hello world, {input}!'}
+
+
+def example_exception_handler(exc: Exception, context: JobContext, controller: TaskStatusController) -> None:
+    print(exc)
+    print(context)
+    controller.error(f'Failed to run task {context.type}. Reason: {exc}')
+
+
+task = Task(task_type='example', task_handler=example_task, exception_handler=example_exception_handler)
+
+worker = ZeebeWorker()  # Will use environment variable ZEEBE_ADDRESS or localhost:26500
+
+worker.add_task(task)
+
+if __name__ == '__main__':
+    worker.work()
