b2cd839a215cda28c1dbfc682808105a4ce7e6ca
Jonatan Martens <jonatanmartenstav@gmail.com>
2020-09-12 14:05:01 +0300

    [FIXED] github action name, dependencies, test files

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyzeebe",
-    version="1.1.0",
+    version="1.1.1",
     author="Jonatan Martens",
     author_email="jonatanmartenstav@gmail.com",
     description="Zeebe client api",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JonatanMartens/pyzeebe",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=["grpcio~=1.31.0", "protobuf~=3.13.0"],
+    exclude=["*test", "tests", "*.bpmn"],
+    keywords="zeebe workflow workflow-engine",
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
