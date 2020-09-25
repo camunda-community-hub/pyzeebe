859f35ebad37e6f545854c9bb255f44357a74256
Jonatan Martens <jonatanmartenstav@gmail.com>
2020-09-25 23:37:00 +0300

    [FIXED] OAuthCredentials not exported

--- pyzeebe/__init__.py
+++ pyzeebe/__init__.py
@@ -1,6 +1,7 @@
 from pyzeebe.client.client import ZeebeClient
 from pyzeebe.common import exceptions
 from pyzeebe.credentials.camunda_cloud_credentials import CamundaCloudCredentials
+from pyzeebe.credentials.oauth_credentials import OAuthCredentials
 from pyzeebe.task.task import Task
 from pyzeebe.task.task_context import TaskContext
 from pyzeebe.task.task_status_controller import TaskStatusController
