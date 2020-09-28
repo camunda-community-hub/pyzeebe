083850b4a1d4087bb083f6a20718fb758e1b7d28
Jonatan Martens <jonatanmartenstav@gmail.com>
2020-09-28 21:40:43 +0300

    [FIXED] TaskDecorator and ZeebeTaskRouter not in exports

--- pyzeebe/__init__.py
+++ pyzeebe/__init__.py
@@ -4,4 +4,6 @@ from pyzeebe.credentials.camunda_cloud_credentials import CamundaCloudCredential
 from pyzeebe.credentials.oauth_credentials import OAuthCredentials
 from pyzeebe.job.job import Job
 from pyzeebe.task.exception_handler import ExceptionHandler
+from pyzeebe.task.task_decorator import TaskDecorator
+from pyzeebe.worker.task_router import ZeebeTaskRouter
 from pyzeebe.worker.worker import ZeebeWorker
