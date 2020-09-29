90b75f994e099756dbb1a0958f0a8b5b8c9ef133
Jonatan Martens <jonatanmartenstav@gmail.com>
2020-09-29 11:42:51 +0300

    [FIXED] JobStatus not exported

--- pyzeebe/__init__.py
+++ pyzeebe/__init__.py
@@ -3,6 +3,7 @@ from pyzeebe.common import exceptions
 from pyzeebe.credentials.camunda_cloud_credentials import CamundaCloudCredentials
 from pyzeebe.credentials.oauth_credentials import OAuthCredentials
 from pyzeebe.job.job import Job
+from pyzeebe.job.job_status import JobStatus
 from pyzeebe.task.exception_handler import ExceptionHandler
 from pyzeebe.task.task_decorator import TaskDecorator
 from pyzeebe.worker.task_router import ZeebeTaskRouter
