7a4fe08a7275b0117cd94ec9026a51d8422512b7
JonatanMartens <jonatanmartenstav@gmail.com>
2021-01-28 12:55:55 +0200

    Use ZeebeAdapter fixture in message and workflow tests

--- tests/unit/grpc_internals/zeebe_message_adapter_test.py
+++ tests/unit/grpc_internals/zeebe_message_adapter_test.py
@@ -7,63 +7,52 @@ import pytest
 from zeebe_grpc.gateway_pb2 import *
 
 from pyzeebe.exceptions import MessageAlreadyExists
-from pyzeebe.grpc_internals.zeebe_message_adapter import ZeebeMessageAdapter
 from tests.unit.utils.grpc_utils import GRPCStatusCode
 from tests.unit.utils.random_utils import RANDOM_RANGE
 
-zeebe_message_adapter: ZeebeMessageAdapter
 
-
-@pytest.fixture(autouse=True)
-def run_around_tests(grpc_channel):
-    global zeebe_message_adapter
-    zeebe_message_adapter = ZeebeMessageAdapter(channel=grpc_channel)
-    yield
-    zeebe_message_adapter = ZeebeMessageAdapter(channel=grpc_channel)
-
-
-def test_publish_message():
-    response = zeebe_message_adapter.publish_message(name=str(uuid4()), variables={}, correlation_key=str(uuid4()),
-                                                     time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
+def test_publish_message(zeebe_adapter):
+    response = zeebe_adapter.publish_message(name=str(uuid4()), variables={}, correlation_key=str(uuid4()),
+                                             time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
     assert isinstance(response, PublishMessageResponse)
 
 
-def test_punlish_message_invalid_name():
+def test_publish_message_invalid_name(zeebe_adapter):
     with pytest.raises(TypeError):
-        zeebe_message_adapter.publish_message(name=randint(0, RANDOM_RANGE), variables={}, correlation_key=str(uuid4()),
-                                              time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
+        zeebe_adapter.publish_message(name=randint(0, RANDOM_RANGE), variables={}, correlation_key=str(uuid4()),
+                                      time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
 
 
-def test_publish_message_invalid_correlation_key():
+def test_publish_message_invalid_correlation_key(zeebe_adapter):
     with pytest.raises(TypeError):
-        zeebe_message_adapter.publish_message(name=str(uuid4()), variables={}, correlation_key=randint(0, RANDOM_RANGE),
-                                              time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
+        zeebe_adapter.publish_message(name=str(uuid4()), variables={}, correlation_key=randint(0, RANDOM_RANGE),
+                                      time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
 
 
-def test_punlish_message_invalid_time_to_live():
+def test_punlish_message_invalid_time_to_live(zeebe_adapter):
     with pytest.raises(TypeError):
-        zeebe_message_adapter.publish_message(name=str(uuid4()), variables={}, correlation_key=str(uuid4()),
-                                              time_to_live_in_milliseconds=str(uuid4()))
+        zeebe_adapter.publish_message(name=str(uuid4()), variables={}, correlation_key=str(uuid4()),
+                                      time_to_live_in_milliseconds=str(uuid4()))
 
 
-def test_publish_message_already_exists():
+def test_publish_message_already_exists(zeebe_adapter):
     message_id = str(uuid4())
     with pytest.raises(MessageAlreadyExists):
-        zeebe_message_adapter.publish_message(message_id=message_id, name=str(uuid4()), variables={},
-                                              correlation_key=str(uuid4()),
-                                              time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
-        zeebe_message_adapter.publish_message(message_id=message_id, name=str(uuid4()), variables={},
-                                              correlation_key=str(uuid4()),
-                                              time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
+        zeebe_adapter.publish_message(message_id=message_id, name=str(uuid4()), variables={},
+                                      correlation_key=str(uuid4()),
+                                      time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
+        zeebe_adapter.publish_message(message_id=message_id, name=str(uuid4()), variables={},
+                                      correlation_key=str(uuid4()),
+                                      time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
 
 
-def test_publish_message_common_errors_called():
-    zeebe_message_adapter._common_zeebe_grpc_errors = MagicMock()
+def test_publish_message_common_errors_called(zeebe_adapter):
+    zeebe_adapter._common_zeebe_grpc_errors = MagicMock()
     error = grpc.RpcError()
     error._state = GRPCStatusCode(grpc.StatusCode.INTERNAL)
 
-    zeebe_message_adapter._gateway_stub.PublishMessage = MagicMock(side_effect=error)
-    zeebe_message_adapter.publish_message(name=str(uuid4()), variables={}, correlation_key=str(uuid4()),
-                                          time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
+    zeebe_adapter._gateway_stub.PublishMessage = MagicMock(side_effect=error)
+    zeebe_adapter.publish_message(name=str(uuid4()), variables={}, correlation_key=str(uuid4()),
+                                  time_to_live_in_milliseconds=randint(0, RANDOM_RANGE))
 
-    zeebe_message_adapter._common_zeebe_grpc_errors.assert_called()
+    zeebe_adapter._common_zeebe_grpc_errors.assert_called()
--- tests/unit/grpc_internals/zeebe_workflow_adapter_test.py
+++ tests/unit/grpc_internals/zeebe_workflow_adapter_test.py
@@ -8,161 +8,150 @@ import pytest
 
 from pyzeebe.exceptions import InvalidJSON, WorkflowNotFound, WorkflowInstanceNotFound, WorkflowHasNoStartEvent, \
     WorkflowInvalid
-from pyzeebe.grpc_internals.zeebe_workflow_adapter import ZeebeWorkflowAdapter
 from tests.unit.utils.grpc_utils import GRPCStatusCode
 from tests.unit.utils.random_utils import RANDOM_RANGE
 
-zeebe_workflow_adapter: ZeebeWorkflowAdapter
 
-
-@pytest.fixture(autouse=True)
-def run_around_tests(grpc_channel):
-    global zeebe_workflow_adapter
-    zeebe_workflow_adapter = ZeebeWorkflowAdapter(channel=grpc_channel)
-    yield
-    zeebe_workflow_adapter = ZeebeWorkflowAdapter(channel=grpc_channel)
-
-
-def test_create_workflow_instance(grpc_servicer):
+def test_create_workflow_instance(grpc_servicer, zeebe_adapter):
     bpmn_process_id = str(uuid4())
     version = randint(0, 10)
     grpc_servicer.mock_deploy_workflow(bpmn_process_id, version, [])
-    response = zeebe_workflow_adapter.create_workflow_instance(bpmn_process_id=bpmn_process_id, variables={},
-                                                               version=version)
+    response = zeebe_adapter.create_workflow_instance(bpmn_process_id=bpmn_process_id, variables={},
+                                                      version=version)
     assert isinstance(response, int)
 
 
-def test_create_workflow_instance_common_errors_called(grpc_servicer):
-    zeebe_workflow_adapter._common_zeebe_grpc_errors = MagicMock()
+def test_create_workflow_instance_common_errors_called(zeebe_adapter):
+    zeebe_adapter._common_zeebe_grpc_errors = MagicMock()
     error = grpc.RpcError()
     error._state = GRPCStatusCode(grpc.StatusCode.INTERNAL)
 
-    zeebe_workflow_adapter._gateway_stub.CreateWorkflowInstance = MagicMock(side_effect=error)
+    zeebe_adapter._gateway_stub.CreateWorkflowInstance = MagicMock(side_effect=error)
 
-    zeebe_workflow_adapter.create_workflow_instance(bpmn_process_id=str(uuid4()), variables={},
-                                                    version=randint(0, 10))
+    zeebe_adapter.create_workflow_instance(bpmn_process_id=str(uuid4()), variables={},
+                                           version=randint(0, 10))
 
-    zeebe_workflow_adapter._common_zeebe_grpc_errors.assert_called()
+    zeebe_adapter._common_zeebe_grpc_errors.assert_called()
 
 
-def test_create_workflow_instance_with_result(grpc_servicer):
+def test_create_workflow_instance_with_result(grpc_servicer, zeebe_adapter):
     bpmn_process_id = str(uuid4())
     version = randint(0, 10)
     grpc_servicer.mock_deploy_workflow(bpmn_process_id, version, [])
-    response = zeebe_workflow_adapter.create_workflow_instance_with_result(bpmn_process_id=bpmn_process_id,
-                                                                           variables={},
-                                                                           version=version, timeout=0,
-                                                                           variables_to_fetch=[])
+    response = zeebe_adapter.create_workflow_instance_with_result(bpmn_process_id=bpmn_process_id,
+                                                                  variables={},
+                                                                  version=version, timeout=0,
+                                                                  variables_to_fetch=[])
     assert isinstance(response, dict)
 
 
-def test_create_workflow_instance_with_result_common_errors_called():
-    zeebe_workflow_adapter._common_zeebe_grpc_errors = MagicMock()
+def test_create_workflow_instance_with_result_common_errors_called(zeebe_adapter):
+    zeebe_adapter._common_zeebe_grpc_errors = MagicMock()
     error = grpc.RpcError()
     error._state = GRPCStatusCode(grpc.StatusCode.INTERNAL)
 
-    zeebe_workflow_adapter._gateway_stub.CreateWorkflowInstanceWithResult = MagicMock(side_effect=error)
+    zeebe_adapter._gateway_stub.CreateWorkflowInstanceWithResult = MagicMock(side_effect=error)
 
-    zeebe_workflow_adapter.create_workflow_instance_with_result(bpmn_process_id=str(uuid4()), variables={},
-                                                                version=randint(0, 10), timeout=0,
-                                                                variables_to_fetch=[])
+    zeebe_adapter.create_workflow_instance_with_result(bpmn_process_id=str(uuid4()), variables={},
+                                                       version=randint(0, 10), timeout=0,
+                                                       variables_to_fetch=[])
 
-    zeebe_workflow_adapter._common_zeebe_grpc_errors.assert_called()
+    zeebe_adapter._common_zeebe_grpc_errors.assert_called()
 
 
-def test_cancel_workflow(grpc_servicer):
+def test_cancel_workflow(grpc_servicer, zeebe_adapter):
     bpmn_process_id = str(uuid4())
     version = randint(0, 10)
     grpc_servicer.mock_deploy_workflow(bpmn_process_id, version, [])
-    workflow_instance_key = zeebe_workflow_adapter.create_workflow_instance(bpmn_process_id=bpmn_process_id,
-                                                                            variables={}, version=version)
-    zeebe_workflow_adapter.cancel_workflow_instance(workflow_instance_key=workflow_instance_key)
+    workflow_instance_key = zeebe_adapter.create_workflow_instance(bpmn_process_id=bpmn_process_id,
+                                                                   variables={}, version=version)
+    zeebe_adapter.cancel_workflow_instance(workflow_instance_key=workflow_instance_key)
     assert workflow_instance_key not in grpc_servicer.active_workflows.keys()
 
 
-def test_cancel_workflow_instance_already_cancelled():
+def test_cancel_workflow_instance_already_cancelled(zeebe_adapter):
     error = grpc.RpcError()
     error._state = GRPCStatusCode(grpc.StatusCode.NOT_FOUND)
 
-    zeebe_workflow_adapter._gateway_stub.CancelWorkflowInstance = MagicMock(side_effect=error)
+    zeebe_adapter._gateway_stub.CancelWorkflowInstance = MagicMock(side_effect=error)
 
     with pytest.raises(WorkflowInstanceNotFound):
-        zeebe_workflow_adapter.cancel_workflow_instance(workflow_instance_key=randint(0, RANDOM_RANGE))
+        zeebe_adapter.cancel_workflow_instance(workflow_instance_key=randint(0, RANDOM_RANGE))
 
 
-def test_cancel_workflow_instance_common_errors_called():
-    zeebe_workflow_adapter._common_zeebe_grpc_errors = MagicMock()
+def test_cancel_workflow_instance_common_errors_called(zeebe_adapter):
+    zeebe_adapter._common_zeebe_grpc_errors = MagicMock()
     error = grpc.RpcError()
     error._state = GRPCStatusCode(grpc.StatusCode.INTERNAL)
 
-    zeebe_workflow_adapter._gateway_stub.CancelWorkflowInstance = MagicMock(side_effect=error)
+    zeebe_adapter._gateway_stub.CancelWorkflowInstance = MagicMock(side_effect=error)
 
-    zeebe_workflow_adapter.cancel_workflow_instance(workflow_instance_key=randint(0, RANDOM_RANGE))
+    zeebe_adapter.cancel_workflow_instance(workflow_instance_key=randint(0, RANDOM_RANGE))
 
-    zeebe_workflow_adapter._common_zeebe_grpc_errors.assert_called()
+    zeebe_adapter._common_zeebe_grpc_errors.assert_called()
 
 
-def test_deploy_workflow_workflow_invalid(grpc_servicer):
+def test_deploy_workflow_workflow_invalid(zeebe_adapter):
     with patch("builtins.open") as mock_open:
         mock_open.return_value = BytesIO()
 
         error = grpc.RpcError()
         error._state = GRPCStatusCode(grpc.StatusCode.INVALID_ARGUMENT)
 
-        zeebe_workflow_adapter._gateway_stub.DeployWorkflow = MagicMock(side_effect=error)
+        zeebe_adapter._gateway_stub.DeployWorkflow = MagicMock(side_effect=error)
 
         with pytest.raises(WorkflowInvalid):
-            zeebe_workflow_adapter.deploy_workflow()
+            zeebe_adapter.deploy_workflow()
 
 
-def test_deploy_workflow_common_errors_called(grpc_servicer):
+def test_deploy_workflow_common_errors_called(zeebe_adapter):
     with patch("builtins.open") as mock_open:
         mock_open.return_value = BytesIO()
 
-        zeebe_workflow_adapter._common_zeebe_grpc_errors = MagicMock()
+        zeebe_adapter._common_zeebe_grpc_errors = MagicMock()
         error = grpc.RpcError()
         error._state = GRPCStatusCode(grpc.StatusCode.INTERNAL)
 
-        zeebe_workflow_adapter._gateway_stub.DeployWorkflow = MagicMock(side_effect=error)
+        zeebe_adapter._gateway_stub.DeployWorkflow = MagicMock(side_effect=error)
 
-        zeebe_workflow_adapter.deploy_workflow()
+        zeebe_adapter.deploy_workflow()
 
-        zeebe_workflow_adapter._common_zeebe_grpc_errors.assert_called()
+        zeebe_adapter._common_zeebe_grpc_errors.assert_called()
 
 
-def test_get_workflow_request_object():
+def test_get_workflow_request_object(zeebe_adapter):
     with patch("builtins.open") as mock_open:
         mock_open.return_value = BytesIO()
         file_path = str(uuid4())
-        zeebe_workflow_adapter._get_workflow_request_object(file_path)
+        zeebe_adapter._get_workflow_request_object(file_path)
         mock_open.assert_called_with(file_path, "rb")
 
 
-def test_create_workflow_errors_not_found():
+def test_create_workflow_errors_not_found(zeebe_adapter):
     error = grpc.RpcError()
     error._state = GRPCStatusCode(grpc.StatusCode.NOT_FOUND)
     with pytest.raises(WorkflowNotFound):
-        zeebe_workflow_adapter._create_workflow_errors(error, str(uuid4()), randint(0, 10, ), {})
+        zeebe_adapter._create_workflow_errors(error, str(uuid4()), randint(0, 10, ), {})
 
 
-def test_create_workflow_errors_invalid_json():
+def test_create_workflow_errors_invalid_json(zeebe_adapter):
     error = grpc.RpcError()
     error._state = GRPCStatusCode(grpc.StatusCode.INVALID_ARGUMENT)
     with pytest.raises(InvalidJSON):
-        zeebe_workflow_adapter._create_workflow_errors(error, str(uuid4()), randint(0, 10, ), {})
+        zeebe_adapter._create_workflow_errors(error, str(uuid4()), randint(0, 10, ), {})
 
 
-def test_create_workflow_errors_workflow_has_no_start_event():
+def test_create_workflow_errors_workflow_has_no_start_event(zeebe_adapter):
     error = grpc.RpcError()
     error._state = GRPCStatusCode(grpc.StatusCode.FAILED_PRECONDITION)
     with pytest.raises(WorkflowHasNoStartEvent):
-        zeebe_workflow_adapter._create_workflow_errors(error, str(uuid4()), randint(0, 10, ), {})
+        zeebe_adapter._create_workflow_errors(error, str(uuid4()), randint(0, 10, ), {})
 
 
-def test_create_workflow_errors_common_errors_called():
-    zeebe_workflow_adapter._common_zeebe_grpc_errors = MagicMock()
+def test_create_workflow_errors_common_errors_called(zeebe_adapter):
+    zeebe_adapter._common_zeebe_grpc_errors = MagicMock()
     error = grpc.RpcError()
     error._state = GRPCStatusCode("test")
-    zeebe_workflow_adapter._create_workflow_errors(error, str(uuid4()), randint(0, 10, ), {})
+    zeebe_adapter._create_workflow_errors(error, str(uuid4()), randint(0, 10, ), {})
 
-    zeebe_workflow_adapter._common_zeebe_grpc_errors.assert_called()
+    zeebe_adapter._common_zeebe_grpc_errors.assert_called()
