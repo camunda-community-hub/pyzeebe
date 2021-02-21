d02ae2badc6b396606d521eb0c67a7f62a625452
JonatanMartens <jonatanmartenstav@gmail.com>
2021-02-21 22:27:44 +0200

    Refactor integration tests to use pytest fixtures and use new ZeebeWorker

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -4,60 +4,64 @@ from uuid import uuid4
 
 import pytest
 
-from pyzeebe import ZeebeWorker, ZeebeClient, Job
+from pyzeebe import ZeebeClient, ZeebeWorker, Job, TaskConfig
 from pyzeebe.exceptions import WorkflowNotFound
 
-zeebe_client: ZeebeClient
-zeebe_worker = ZeebeWorker()
+
+@pytest.fixture(scope="session")
+def zeebe_client():
+    return ZeebeClient()
 
 
-def exception_handler(exc: Exception, job: Job) -> None:
-    job.set_error_status(f"Failed to run task {job.type}. Reason: {exc}")
+@pytest.fixture(scope="session")
+def zeebe_worker():
+    worker = ZeebeWorker()
 
+    def exception_handler(exc: Exception, job: Job) -> None:
+        job.set_error_status(f"Failed to run task {job.type}. Reason: {exc}")
 
-@zeebe_worker.task(task_type="test", exception_handler=exception_handler)
-def task_handler(should_throw: bool, input: str) -> Dict:
-    if should_throw:
-        raise Exception("Error thrown")
-    else:
-        return {"output": input + str(uuid4())}
+    @worker.task(TaskConfig("test", exception_handler))
+    def task_handler(should_throw: bool, input: str) -> Dict:
+        if should_throw:
+            raise Exception("Error thrown")
+        else:
+            return {"output": input + str(uuid4())}
+
+    return worker
 
 
 @pytest.fixture(scope="module", autouse=True)
-def setup():
-    global zeebe_client, task_handler
-
+def setup(zeebe_worker, zeebe_client):
     zeebe_worker.work(watch=True)
 
-    zeebe_client = ZeebeClient()
     try:
         integration_tests_path = os.path.join("tests", "integration")
         zeebe_client.deploy_workflow(os.path.join(integration_tests_path, "test.bpmn"))
     except FileNotFoundError:
         zeebe_client.deploy_workflow("test.bpmn")
 
-    yield zeebe_client
+    yield
     zeebe_worker.stop(wait=True)
     assert not zeebe_worker._watcher_thread.is_alive()
 
 
-def test_run_workflow():
+def test_run_workflow(zeebe_client: ZeebeClient):
     workflow_key = zeebe_client.run_workflow("test", {"input": str(uuid4()), "should_throw": False})
     assert isinstance(workflow_key, int)
 
 
-def test_non_existent_workflow():
+def test_non_existent_workflow(zeebe_client: ZeebeClient):
     with pytest.raises(WorkflowNotFound):
         zeebe_client.run_workflow(str(uuid4()))
 
 
-def test_run_workflow_with_result():
+def test_run_workflow_with_result(zeebe_client: ZeebeClient):
     input = str(uuid4())
     output = zeebe_client.run_workflow_with_result("test", {"input": input, "should_throw": False})
     assert isinstance(output["output"], str)
     assert output["output"].startswith(input)
 
 
-def test_cancel_workflow():
+def test_cancel_workflow(zeebe_client: ZeebeClient):
     workflow_key = zeebe_client.run_workflow("test", {"input": str(uuid4()), "should_throw": False})
     zeebe_client.cancel_workflow_instance(workflow_key)
