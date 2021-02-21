10574650d44425f894d0ee0ed6801bd11c448f44
JonatanMartens <jonatanmartenstav@gmail.com>
2021-02-21 21:18:09 +0200

    Add type annotations to ZeebeWorker tests

--- tests/unit/worker/worker_test.py
+++ tests/unit/worker/worker_test.py
@@ -1,10 +1,12 @@
 import time
+from threading import Event as StopEvent
+from typing import List
 from unittest.mock import patch, MagicMock
 from uuid import uuid4
 
 import pytest
 
-from pyzeebe import TaskDecorator
+from pyzeebe import TaskDecorator, ZeebeTaskRouter
 from pyzeebe.exceptions import DuplicateTaskType, MaxConsecutiveTaskThreadError
 from pyzeebe.job.job import Job
 from pyzeebe.task.task import Task
@@ -12,24 +14,24 @@ from pyzeebe.worker.worker import ZeebeWorker
 
 
 class TestAddTask:
-    def test_add_task(self, zeebe_worker, task):
+    def test_add_task(self, zeebe_worker: ZeebeWorker, task: Task):
         zeebe_worker._add_task(task)
 
         assert zeebe_worker.get_task(task.type) == task
 
-    def test_raises_on_duplicate(self, zeebe_worker, task):
+    def test_raises_on_duplicate(self, zeebe_worker: ZeebeWorker, task: Task):
         zeebe_worker._add_task(task)
         with pytest.raises(DuplicateTaskType):
             zeebe_worker._add_task(task)
 
-    def test_only_one_task_added(self, zeebe_worker):
+    def test_only_one_task_added(self, zeebe_worker: ZeebeWorker):
         @zeebe_worker.task(str(uuid4()))
         def dummy_function():
             pass
 
         assert len(zeebe_worker.tasks) == 1
 
-    def test_task_type_saved(self, zeebe_worker, task):
+    def test_task_type_saved(self, zeebe_worker: ZeebeWorker, task: Task):
         zeebe_worker._add_task(task)
 
         assert zeebe_worker.get_task(task.type).type == task.type
@@ -45,22 +47,22 @@ class TestAddTask:
 
 
 class TestDecorator:
-    def test_add_before_decorator(self, zeebe_worker, decorator):
+    def test_add_before_decorator(self, zeebe_worker: ZeebeWorker, decorator: TaskDecorator):
         zeebe_worker.before(decorator)
         assert len(zeebe_worker._before) == 1
         assert decorator in zeebe_worker._before
 
-    def test_add_after_decorator(self, zeebe_worker, decorator):
+    def test_add_after_decorator(self, zeebe_worker: ZeebeWorker, decorator: TaskDecorator):
         zeebe_worker.after(decorator)
         assert len(zeebe_worker._after) == 1
         assert decorator in zeebe_worker._after
 
-    def test_add_constructor_before_decorator(self, decorator):
+    def test_add_constructor_before_decorator(self, decorator: TaskDecorator):
         zeebe_worker = ZeebeWorker(before=[decorator])
         assert len(zeebe_worker._before) == 1
         assert decorator in zeebe_worker._before
 
-    def test_add_constructor_after_decorator(self, decorator):
+    def test_add_constructor_after_decorator(self, decorator: TaskDecorator):
         zeebe_worker = ZeebeWorker(after=[decorator])
         assert len(zeebe_worker._after) == 1
         assert decorator in zeebe_worker._after
@@ -83,29 +85,30 @@ class TestDecorator:
 
 class TestHandleJobs:
     @pytest.fixture(autouse=True)
-    def get_jobs_mock(self, zeebe_worker):
+    def get_jobs_mock(self, zeebe_worker: ZeebeWorker):
         zeebe_worker._get_jobs = MagicMock()
         return zeebe_worker._get_jobs
 
     @pytest.fixture(autouse=True)
-    def task_handler_mock(self, task):
+    def task_handler_mock(self, task: Task):
         task.job_handler = MagicMock(wraps=task.job_handler)
 
-    def test_handle_no_job(self, zeebe_worker, task, get_jobs_mock):
+    def test_handle_no_job(self, zeebe_worker: ZeebeWorker, task: Task, get_jobs_mock: MagicMock):
         get_jobs_mock.return_value = []
 
         zeebe_worker._handle_jobs(task)
 
         task.job_handler.assert_not_called()
 
-    def test_handle_one_job(self, zeebe_worker, task, job_from_task, get_jobs_mock):
+    def test_handle_one_job(self, zeebe_worker: ZeebeWorker, task: Task, job_from_task: Job, get_jobs_mock: MagicMock):
         get_jobs_mock.return_value = [job_from_task]
 
         zeebe_worker._handle_jobs(task)
 
         task.job_handler.assert_called_with(job_from_task)
 
-    def test_handle_many_jobs(self, zeebe_worker, task, job_from_task, get_jobs_mock):
+    def test_handle_many_jobs(self, zeebe_worker: ZeebeWorker, task: Task, job_from_task: Job,
+                              get_jobs_mock: MagicMock):
         get_jobs_mock.return_value = [job_from_task] * 10
 
         zeebe_worker._handle_jobs(task)
@@ -114,7 +117,7 @@ class TestHandleJobs:
 
 
 class TestWorkerThreads:
-    def test_work_thread_start_called(self, zeebe_worker, task):
+    def test_work_thread_start_called(self, zeebe_worker: ZeebeWorker, task: Task):
         with patch("pyzeebe.worker.worker.Thread") as thread_mock:
             thread_instance_mock = MagicMock()
             thread_mock.return_value = thread_instance_mock
@@ -123,12 +126,13 @@ class TestWorkerThreads:
             zeebe_worker.stop()
             thread_instance_mock.start.assert_called_once()
 
-    def test_stop_worker(self, zeebe_worker):
+    def test_stop_worker(self, zeebe_worker: ZeebeWorker):
         zeebe_worker.work()
         zeebe_worker.stop()
 
     def test_watch_task_threads_dont_restart_running_threads(
-            self, zeebe_worker, task, handle_task_mock, stop_event_mock, handle_not_alive_thread_spy, stop_after_test):
+            self, zeebe_worker: ZeebeWorker, task: Task, handle_task_mock: MagicMock, stop_event_mock: MagicMock,
+            handle_not_alive_thread_spy: MagicMock, stop_after_test: StopEvent):
         def fake_task_handler_never_return(*_args):
             while not stop_after_test.is_set():
                 time.sleep(0.05)
@@ -144,7 +148,8 @@ class TestWorkerThreads:
         assert handle_not_alive_thread_spy.call_count == 0
 
     def test_watch_task_threads_that_die_get_restarted_then_exit_after_too_many_errors(
-            self, zeebe_worker, task, handle_task_mock, stop_event_mock, handle_not_alive_thread_spy):
+            self, zeebe_worker: ZeebeWorker, task: Task, handle_task_mock: MagicMock, stop_event_mock: MagicMock,
+            handle_not_alive_thread_spy: MagicMock):
         def fake_task_handler_return_immediately(*_args):
             pass
 
@@ -173,18 +178,19 @@ class TestGetJobs:
 
 
 class TestIncludeRouter:
-    def test_include_router_adds_task(self, zeebe_worker, router, task_type):
+    def test_include_router_adds_task(self, zeebe_worker: ZeebeWorker, router: ZeebeTaskRouter, task_type: str):
         self.include_router_with_task(zeebe_worker, router, task_type)
 
         assert zeebe_worker.get_task(task_type) is not None
 
-    def test_include_multiple_routers(self, zeebe_worker, routers):
+    def test_include_multiple_routers(self, zeebe_worker: ZeebeWorker, routers: List[ZeebeTaskRouter]):
         for router in routers:
             self.include_router_with_task(zeebe_worker, router)
 
         assert len(zeebe_worker.tasks) == len(routers)
 
-    def test_router_before_decorator(self, zeebe_worker, router, decorator, mocked_job_with_adapter):
+    def test_router_before_decorator(self, zeebe_worker: ZeebeWorker, router: ZeebeTaskRouter, decorator: TaskDecorator,
+                                     mocked_job_with_adapter: Job):
         router.before(decorator)
         task = self.include_router_with_task(zeebe_worker, router)
 
@@ -192,7 +198,8 @@ class TestIncludeRouter:
 
         decorator.assert_called_once()
 
-    def test_router_after_decorator(self, zeebe_worker, router, decorator, mocked_job_with_adapter):
+    def test_router_after_decorator(self, zeebe_worker: ZeebeWorker, router: ZeebeTaskRouter, decorator: TaskDecorator,
+                                    mocked_job_with_adapter: Job):
         router.after(decorator)
         task = self.include_router_with_task(zeebe_worker, router)
 
@@ -201,7 +208,7 @@ class TestIncludeRouter:
         decorator.assert_called_once()
 
     @staticmethod
-    def include_router_with_task(zeebe_worker, router, task_type=None):
+    def include_router_with_task(zeebe_worker: ZeebeWorker, router: ZeebeTaskRouter, task_type: str = None) -> Task:
         task_type = task_type or str(uuid4())
 
         @router.task(task_type)
