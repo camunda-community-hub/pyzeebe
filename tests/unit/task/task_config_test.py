f746baad0d2d185fad515a160115bf4d62a620b0
JonatanMartens <jonatanmartenstav@gmail.com>
2021-03-06 18:44:35 +0200

    Add happy path to task_config tests

--- tests/unit/task/task_config_test.py
+++ tests/unit/task/task_config_test.py
@@ -8,6 +8,24 @@ from pyzeebe.errors import NoVariableNameGivenError
 from pyzeebe.task.task_config import TaskConfig, default_exception_handler
 
 
+def test_add_dict_task_with_correct_parameters(task_type: str):
+    expected_config_as_dict = {
+        "type": task_type,
+        "exception_handler": default_exception_handler,
+        "timeout": 10000,
+        "max_jobs_to_activate": 32,
+        "variables_to_fetch": None,
+        "single_value": False,
+        "variable_name": None,
+        "before": [],
+        "after": []
+    }
+
+    config = TaskConfig(task_type)
+
+    assert config.__dict__ == expected_config_as_dict
+
+
 def test_add_non_dict_task_without_variable_name():
     with pytest.raises(NoVariableNameGivenError):
         TaskConfig(str(uuid4()), single_value=True)
