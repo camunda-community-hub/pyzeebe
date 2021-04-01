19eb66df1c952cd28e7b64916ca6ac1ad15aaf62
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-04-02 00:11:12 +0300

    Update job properties to reflect new 1.0.0 api

--- tests/unit/utils/random_utils.py
+++ tests/unit/utils/random_utils.py
@@ -11,8 +11,8 @@ RANDOM_RANGE = 1000000000
 def random_job(task: Task = Task(task_type="test", task_handler=lambda x: {"x": x},
                                  exception_handler=lambda x, y, z: x), zeebe_adapter: ZeebeAdapter = None) -> Job:
     return Job(_type=task.type, key=randint(0, RANDOM_RANGE), worker=str(uuid4()),
-               retries=randint(0, 10), workflow_instance_key=randint(0, RANDOM_RANGE),
-               bpmn_process_id=str(uuid4()), workflow_definition_version=randint(0, 100),
-               workflow_key=randint(0, RANDOM_RANGE), element_id=str(uuid4()),
+               retries=randint(0, 10), process_instance_key=randint(0, RANDOM_RANGE),
+               bpmn_process_id=str(uuid4()), process_definition_version=randint(0, 100),
+               process_definition_key=randint(0, RANDOM_RANGE), element_id=str(uuid4()),
                element_instance_key=randint(0, RANDOM_RANGE), variables={}, custom_headers={},
                deadline=randint(0, RANDOM_RANGE), zeebe_adapter=zeebe_adapter)
