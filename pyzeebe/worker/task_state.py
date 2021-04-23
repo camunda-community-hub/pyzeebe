ff42b953e86699699d640ccce3df3625b5a1563d
Kristoffer Bakkejord <kbakk@users.noreply.github.com>
2021-04-23 19:17:58 +0200

    Use more descriptive instance attribute name

--- pyzeebe/worker/task_state.py
+++ pyzeebe/worker/task_state.py
@@ -5,18 +5,18 @@ logger = logging.getLogger(__name__)
 
 class TaskState:
     def __init__(self):
-        self._data = set()
+        self._active_jobs = set()
 
     def remove(self, job: Job) -> None:
         try:
-            self._data.remove(job.key)
+            self._active_jobs.remove(job.key)
         except KeyError:
             logger.warning(f"Could not find Job key {job.key} when trying to remove from TaskState")
 
     def add(self, job: Job) -> None:
-        if job.key in self._data:
+        if job.key in self._active_jobs:
             raise ValueError(f"Job {job.key} already registered in TaskState")
-        self._data.add(job.key)
+        self._active_jobs.add(job.key)
 
     def count_active(self) -> int:
-        return len(self._data)
+        return len(self._active_jobs)
