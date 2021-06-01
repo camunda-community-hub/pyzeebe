3237e6e1322f0b1fa380d33d3e77c9192bc7425b
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-06-01 22:22:01 +0300

    Add get_job_parameter function to parameter_tools

--- tests/unit/function_tools/parameter_tools_test.py
+++ tests/unit/function_tools/parameter_tools_test.py
@@ -3,6 +3,7 @@ from typing import Callable, List
 import pytest
 
 from pyzeebe.function_tools import parameter_tools
+from pyzeebe.job.job import Job
 from tests.unit.utils import dummy_functions
 
 
@@ -26,3 +27,23 @@ class TestGetFunctionParameters:
     ])
     def test_get_params(self, fn: Callable, expected: List[str]):
         assert parameter_tools.get_parameters_from_function(fn) == expected
+
+
+class TestGetJobParameter:
+    def test_returns_none_when_there_are_no_parameters_annotated_with_job(self):
+        job_parameter = parameter_tools.get_job_parameter_name(
+            dummy_functions.multiple_params)
+
+        assert job_parameter == None
+
+    def test_returns_parameter_name_when_annotated(self):
+        job_parameter = parameter_tools.get_job_parameter_name(
+            dummy_functions.with_job_parameter)
+
+        assert job_parameter == "job"
+
+    def test_returns_first_parameter_annotated_with_job(self):
+        job_parameter = parameter_tools.get_job_parameter_name(
+            dummy_functions.with_multiple_job_parameters)
+
+        assert job_parameter == "job"
