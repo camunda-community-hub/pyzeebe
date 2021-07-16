318980b0900bd15078dc2c7801d029dc9d5843b6
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-07-16 12:33:18 +0300

    Add tests for overidden hostname/port

--- tests/unit/channel/insecure_channel_test.py
+++ tests/unit/channel/insecure_channel_test.py
@@ -1,9 +1,11 @@
+from uuid import uuid4
+
 import grpc
 import pytest
 from mock import Mock, patch
-
 from pyzeebe import create_insecure_channel
 from pyzeebe.channel.channel_options import get_channel_options
+from pyzeebe.channel.utils import DEFAULT_HOSTNAME, DEFAULT_PORT
 
 
 class TestCreateInsecureChannel:
@@ -20,5 +22,21 @@ class TestCreateInsecureChannel:
     def test_calls_using_default_grpc_options(self, insecure_channel_mock: Mock):
         create_insecure_channel()
 
-        insecure_channel_call = insecure_channel_mock.call_args[1]
-        assert insecure_channel_call["options"] == get_channel_options()
+        insecure_channel_call = insecure_channel_mock.mock_calls[0]
+        assert insecure_channel_call.kwargs["options"] == get_channel_options()
+
+    def test_overrides_default_port_if_provided(self, insecure_channel_mock: Mock):
+        port = 123
+
+        create_insecure_channel(port=port)
+
+        insecure_channel_call = insecure_channel_mock.mock_calls[0]
+        assert insecure_channel_call.args[0] == f"{DEFAULT_HOSTNAME}:{port}"
+
+    def test_overrides_default_hostname_if_provided(self, insecure_channel_mock: Mock):
+        hostname = str(uuid4())
+
+        create_insecure_channel(hostname=hostname)
+
+        insecure_channel_call = insecure_channel_mock.mock_calls[0]
+        assert insecure_channel_call.args[0] == f"{hostname}:{DEFAULT_PORT}"
--- tests/unit/channel/secure_channel_test.py
+++ tests/unit/channel/secure_channel_test.py
@@ -1,9 +1,11 @@
+from uuid import uuid4
+
 import grpc
 import pytest
 from mock import Mock, patch
-
 from pyzeebe import create_secure_channel
 from pyzeebe.channel.channel_options import get_channel_options
+from pyzeebe.channel.utils import DEFAULT_HOSTNAME, DEFAULT_PORT
 
 
 class TestCreateSecureChannel:
@@ -26,5 +28,21 @@ class TestCreateSecureChannel:
     def test_calls_using_default_grpc_options(self, secure_channel_mock: Mock):
         create_secure_channel()
 
-        secure_channel_call = secure_channel_mock.call_args[1]
-        assert secure_channel_call["options"] == get_channel_options()
+        secure_channel_call = secure_channel_mock.mock_calls[0]
+        assert secure_channel_call.kwargs["options"] == get_channel_options()
+
+    def test_overrides_default_port_if_provided(self, secure_channel_mock: Mock):
+        port = 123
+
+        create_secure_channel(port=port)
+
+        secure_channel_call = secure_channel_mock.mock_calls[0]
+        assert secure_channel_call.args[0] == f"{DEFAULT_HOSTNAME}:{port}"
+
+    def test_overrides_default_hostname_if_provided(self, secure_channel_mock: Mock):
+        hostname = str(uuid4())
+
+        create_secure_channel(hostname=hostname)
+
+        secure_channel_call = secure_channel_mock.mock_calls[0]
+        assert secure_channel_call.args[0] == f"{hostname}:{DEFAULT_PORT}"
