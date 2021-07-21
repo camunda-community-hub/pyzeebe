b8cbf15aae3ee728f229d4bee698b4f278436362
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-07-21 15:56:03 +0300

    Make integration test fixtures module scoped
    This way unit tests will not use them

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -10,29 +10,29 @@ from pyzeebe import Job, ZeebeClient, ZeebeWorker, create_insecure_channel
 from pyzeebe.errors import ProcessDefinitionNotFoundError
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def event_loop():
     loop = asyncio.new_event_loop()
     yield loop
     loop.close()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def grpc_channel():
     return create_insecure_channel()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def zeebe_client(grpc_channel: grpc.aio.Channel):
     return ZeebeClient(grpc_channel)
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def zeebe_worker(grpc_channel):
     return ZeebeWorker(grpc_channel)
 
 
-@pytest.fixture(autouse=True, scope="session")
+@pytest.fixture(autouse=True, scope="module")
 def task(zeebe_worker: ZeebeWorker):
     def exception_handler(exc: Exception, job: Job) -> None:
         job.set_error_status(f"Failed to run task {job.type}. Reason: {exc}")
@@ -46,7 +46,7 @@ def task(zeebe_worker: ZeebeWorker):
 
 
 @pytest.mark.asyncio
-@pytest.fixture(autouse=True, scope="session")
+@pytest.fixture(autouse=True, scope="module")
 async def deploy_process(zeebe_client: ZeebeClient):
     try:
         integration_tests_path = os.path.join("tests", "integration")
@@ -57,7 +57,7 @@ async def deploy_process(zeebe_client: ZeebeClient):
         await zeebe_client.deploy_process("test.bpmn")
 
 
-@pytest.fixture(autouse=True, scope="session")
+@pytest.fixture(autouse=True, scope="module")
 def start_worker(event_loop: asyncio.AbstractEventLoop, zeebe_worker: ZeebeWorker):
     event_loop.create_task(zeebe_worker.work())
     yield
