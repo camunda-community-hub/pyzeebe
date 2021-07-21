8e7eb85dba5d1945598deb03883c778b26ef1a30
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-07-21 17:41:18 +0300

    Improve ProcessTimeoutError message readability

--- pyzeebe/errors/process_errors.py
+++ pyzeebe/errors/process_errors.py
@@ -4,22 +4,23 @@ from pyzeebe.errors.pyzeebe_errors import PyZeebeError
 class ProcessDefinitionNotFoundError(PyZeebeError):
     def __init__(self, bpmn_process_id: str, version: int):
         super().__init__(
-            f"Process definition: {bpmn_process_id}  with {version} was not found")
+            f"Process definition: {bpmn_process_id}  with {version} was not found"
+        )
         self.bpmn_process_id = bpmn_process_id
         self.version = version
 
 
 class ProcessInstanceNotFoundError(PyZeebeError):
     def __init__(self, process_instance_key: int):
-        super().__init__(
-            f"Process instance key: {process_instance_key} was not found")
+        super().__init__(f"Process instance key: {process_instance_key} was not found")
         self.process_instance_key = process_instance_key
 
 
 class ProcessDefinitionHasNoStartEventError(PyZeebeError):
     def __init__(self, bpmn_process_id: str):
         super().__init__(
-            f"Process {bpmn_process_id} has no start event that can be called manually")
+            f"Process {bpmn_process_id} has no start event that can be called manually"
+        )
         self.bpmn_process_id = bpmn_process_id
 
 
@@ -33,5 +34,7 @@ class InvalidJSONError(PyZeebeError):
 
 class ProcessTimeoutError(PyZeebeError, TimeoutError):
     def __init__(self, bpmn_process_id: str):
-        super().__init__(f"Timeout while waiting for {bpmn_process_id} to complete")
+        super().__init__(
+            f"Timeout while waiting for process {bpmn_process_id} to complete"
+        )
         self.bpmn_process_id = bpmn_process_id
