de3c3093281717350781a24b69ed3f85eb1c99ba
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-09-09 20:16:11 +0300

    Allow reactivation of already running job in task state
    See https://github.com/camunda-community-hub/pyzeebe/issues/212.

--- tests/unit/worker/task_state_test.py
+++ tests/unit/worker/task_state_test.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pyzeebe.job.job import Job
 from pyzeebe.worker.task_state import TaskState
 
 
@@ -23,13 +24,15 @@ def test_add_then_remove_results_in_0_active(task_state, job_from_task):
     assert task_state.count_active() == 0
 
 
-def test_prevents_adding_duplicate_job(task_state, job_from_task):
-    with pytest.raises(ValueError) as exc_info:
-        for _ in range(2):
-            task_state.add(job_from_task)
-    assert exc_info.value.args[0].endswith("already registered in TaskState")
-
-
-def test_remove_non_existing_job_dont_withdraw_from_active_jobs(task_state, job_from_task):
+def test_remove_non_existing_job_dont_withdraw_from_active_jobs(
+    task_state, job_from_task
+):
     task_state.remove(job_from_task)
     assert task_state.count_active() == 0
+
+
+def test_add_already_activated_job_does_not_raise_an_error(
+    task_state: TaskState, job_from_task: Job
+):
+    task_state.add(job_from_task)
+    task_state.add(job_from_task)
