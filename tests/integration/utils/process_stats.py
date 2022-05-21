d4617c38229c77328f89c41feea720ba50c5eaeb
Jonatan Martens <jonatanmartenstav@gmail.com>
2022-05-21 16:33:21 +0300

    Poll for process to finish in integration tests

--- tests/integration/utils/process_stats.py
+++ tests/integration/utils/process_stats.py
@@ -1,13 +1,21 @@
 from typing import Dict, List
 
+from .process_run import ProcessRun
+
 
 class ProcessStats:
     def __init__(self, process_name: str):
         self.process_name = process_name
-        self.runs: List[Dict] = []
+        self.runs: List[ProcessRun] = []
 
-    def add_process_run(self, starting_parameters: Dict):
-        self.runs.append({"starting_parameters": starting_parameters})
+    def add_process_run(self, process: ProcessRun):
+        self.runs.append(process)
+
+    def has_process_been_run(self, process_instance_key: int) -> bool:
+        return any(run.instance_key == process_instance_key for run in self.runs if run.instance_key)
+
+    def has_process_with_variables_been_run(self, variables: Dict) -> bool:
+        return any(run.variables == variables for run in self.runs)
 
     def get_process_runs(self) -> int:
         return len(self.runs)
