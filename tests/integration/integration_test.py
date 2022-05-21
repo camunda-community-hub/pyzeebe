37f1a39a93ce3ea2804f96f5a53b346aec2fac13
Jonatan Martens <jonatanmartenstav@gmail.com>
2022-05-21 14:09:37 +0300

    Improve run process tests

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -1,3 +1,4 @@
+from asyncio import sleep
 from typing import Dict
 from uuid import uuid4
 
@@ -5,12 +6,19 @@ import pytest
 
 from pyzeebe import ZeebeClient
 from pyzeebe.errors import ProcessDefinitionNotFoundError
+from tests.integration.utils import ProcessStats
 
 
 @pytest.mark.asyncio
-async def test_run_process(zeebe_client: ZeebeClient, process_name: str, process_variables: Dict):
-    process_key = await zeebe_client.run_process(process_name, process_variables)
-    assert isinstance(process_key, int)
+async def test_run_process(
+    zeebe_client: ZeebeClient, process_name: str, process_variables: Dict, process_stats: ProcessStats
+):
+    initial_amount_of_processes = process_stats.get_process_runs()
+
+    await zeebe_client.run_process(process_name, process_variables)
+    await sleep(0.2)  # Wait for process to finish
+
+    assert process_stats.get_process_runs() == initial_amount_of_processes + 1
 
 
 @pytest.mark.asyncio
@@ -22,8 +30,7 @@ async def test_non_existent_process(zeebe_client: ZeebeClient):
 @pytest.mark.asyncio
 async def test_run_process_with_result(zeebe_client: ZeebeClient, process_name: str, process_variables: Dict):
     process_instance_key, process_result = await zeebe_client.run_process_with_result(process_name, process_variables)
-    assert isinstance(process_instance_key, int)
-    assert isinstance(process_result["output"], str)
+
     assert process_result["output"].startswith(process_variables["input"])
 
 
