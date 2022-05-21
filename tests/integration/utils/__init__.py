7700d0a37edaef42b8aa28f505b3d3c637b2d33a
Jonatan Martens <jonatanmartenstav@gmail.com>
2022-05-21 16:52:06 +0300

    Fix imports

--- tests/integration/utils/__init__.py
+++ tests/integration/utils/__init__.py
@@ -1,3 +1,3 @@
-from .process_stats import ProcessStats
 from .process_run import ProcessRun
+from .process_stats import ProcessStats
 from .wait_for_process import wait_for_process, wait_for_process_with_variables
