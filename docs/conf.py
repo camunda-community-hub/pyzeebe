31165679e07f2ab56e44d2b5d7f2d03e2578d2fd
EyalCMX <eyal.goren@checkmarx.com>
2022-07-17 17:39:19 +0300

    Bump version: 3.0.5 → 3.0.6

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = "2020, Jonatan Martens"
 author = "Jonatan Martens"
 
 # The full version, including alpha/beta/rc tags
-release = "3.0.5"
+release = "3.0.6"
 
 # -- General configuration ---------------------------------------------------
 
@@ -55,6 +55,6 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-version = "3.0.5"
+version = "3.0.6"
 
 master_doc = "index"
--- pyzeebe/__init__.py
+++ pyzeebe/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "3.0.5"
+__version__ = "3.0.6"
 
 from pyzeebe import errors
 from pyzeebe.channel import *
