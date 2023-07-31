6a56c799f18f048b7696a7fa80623c37013dfd4d
Jonatan Martens <jonatanmartenstav@gmail.com>
2023-07-31 14:12:06 +0300

    Increase integration test timeout to reduce flakiness

--- tests/integration/run_process_test.py
+++ tests/integration/run_process_test.py
@@ -8,6 +8,9 @@ from pyzeebe.errors import ProcessDefinitionNotFoundError
 from tests.integration.utils import ProcessStats, wait_for_process
 
 
+PROCESS_TIMEOUT_IN_MS = 60_000
+
+
 async def test_run_process(
     zeebe_client: ZeebeClient, process_name: str, process_variables: Dict, process_stats: ProcessStats
 ):
@@ -25,6 +28,8 @@ async def test_non_existent_process(zeebe_client: ZeebeClient):
 
 
 async def test_run_process_with_result(zeebe_client: ZeebeClient, process_name: str, process_variables: Dict):
-    _, process_result = await zeebe_client.run_process_with_result(process_name, process_variables)
+    _, process_result = await zeebe_client.run_process_with_result(
+        process_name, process_variables, timeout=PROCESS_TIMEOUT_IN_MS
+    )
 
     assert process_result["output"].startswith(process_variables["input"])
